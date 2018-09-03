cfffe6babb06635b56fe7b9def34a062b288c272
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-09-02 20:32:44 -0600

    kept breaking things trying to move forward, break time

--- src/components/myHand.vue
+++ src/components/myHand.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="row" style="justify-content: space-evenly">
       <div class="card col-2" style="width: 18rem;" v-for="card in setGame.players[0].hand">
-        <img class="card-img-top" src="card.img" alt="Card image cap">
+        <img class="card-img-top" src="card.img" alt="Card image cap">{{card.img}}<img/>
         <div class="card-body">
           <!-- <span class='defense'>{{myHand.defense}} </span>
           <span class='attack'>{{myHand.attack}} </span>
@@ -43,6 +43,7 @@
       selectHero() {
         //needs to add the playerCardID to the payload
         //from the @click button on card - update store w/ selected hero
+        //working here
       }
     }
 
